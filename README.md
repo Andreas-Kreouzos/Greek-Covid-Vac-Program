388844ab3c0215f9291229e9b3a1d25cdab67643
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2022-10-22 17:08:43 +0300

    Added POJO explanation in README.MD file, added Lombok

--- README.md
+++ README.md
@@ -20,4 +20,15 @@ view in a HTML Thymeleaf template.
 The project has a very simple implementation of Spring Security, which handles the login
 and logout pages. For that reason, a separate login.html was created for the necessary
 checking of the user credentials. The latter are being created with a @Bean of the class
-UserDetailsService via its builder method.
+UserDetailsService via its builder method. For the creation of the password, the PasswordEncoder
+Class was used.
+
+## POJO Class
+
+For the creation of the POJO Class the next JSON file was needed to be consumed. This file
+contains fields specifically for the Covid-19 Vaccination Initiative which was called "Operation Liberty"
+in Greece. The consumed URL fetches results for the dates between 2022-07-30 to 2022-07-31.
+Of course, these dates could be modified according to the instructions of the API but for reasons
+of demonstration, only these two dates were chosen.
+
+<img width="812" alt="Consuming JSON" src="https://user-images.githubusercontent.com/77160233/197342597-8b8a8e64-065e-42f8-b608-101280cfcc80.png">
--- src/main/java/com/andrekreou/covid/gov/model/Covid.java
+++ src/main/java/com/andrekreou/covid/gov/model/Covid.java
@@ -1,12 +1,18 @@
 package com.andrekreou.covid.gov.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity()
 @Table
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Covid {
     @Id
     @SequenceGenerator(
@@ -19,117 +25,11 @@ public class Covid {
             generator = "covid_sequence"
     )
     private Integer id;
-
     private String area;
-
     private Integer areaid;
-
     private String referencedate;
-
     private Integer totaldose1;
-
     private Integer totaldose2;
-
     private Integer totaldose3;
-
     private Integer totalvaccinations;
-
-    public Covid() {
-    }
-
-    public Covid(Integer id,
-                 String area,
-                 Integer areaid,
-                 String referencedate,
-                 Integer totaldose1,
-                 Integer totaldose2,
-                 Integer totaldose3,
-                 Integer totalvaccinations) {
-        this.id = id;
-        this.area = area;
-        this.areaid = areaid;
-        this.referencedate = referencedate;
-        this.totaldose1 = totaldose1;
-        this.totaldose2 = totaldose2;
-        this.totaldose3 = totaldose3;
-        this.totalvaccinations = totalvaccinations;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public Integer getAreaid() {
-        return areaid;
-    }
-
-    public void setAreaid(Integer areaid) {
-        this.areaid = areaid;
-    }
-
-    public String getReferencedate() {
-        return referencedate;
-    }
-
-    public void setReferencedate(String referencedate) {
-        this.referencedate = referencedate;
-    }
-
-    public Integer getTotaldose1() {
-        return totaldose1;
-    }
-
-    public void setTotaldose1(Integer totaldose1) {
-        this.totaldose1 = totaldose1;
-    }
-
-    public Integer getTotaldose2() {
-        return totaldose2;
-    }
-
-    public void setTotaldose2(Integer totaldose2) {
-        this.totaldose2 = totaldose2;
-    }
-
-    public Integer getTotaldose3() {
-        return totaldose3;
-    }
-
-    public void setTotaldose3(Integer totaldose3) {
-        this.totaldose3 = totaldose3;
-    }
-
-    public Integer getTotalvaccinations() {
-        return totalvaccinations;
-    }
-
-    public void setTotalvaccinations(Integer totalvaccinations) {
-        this.totalvaccinations = totalvaccinations;
-    }
-
-    @Override
-    public String toString() {
-        return "Covid{" +
-                "id=" + id +
-                ", area='" + area + '\'' +
-                ", area_id=" + areaid +
-                ", referencedate='" + referencedate + '\'' +
-                ", totaldose1=" + totaldose1 +
-                ", totaldose2=" + totaldose2 +
-                ", totaldose3=" + totaldose3 +
-                ", totalvaccinations=" + totalvaccinations +
-                '}';
-    }
 }
